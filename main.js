7135df9f516aa4c273543617bc58a9195eb4396a
HarryNguyen2003 <90954577+HieuFPT2003@users.noreply.github.com>
2022-08-10 16:52:06 +0700

    clean code

--- main.js
+++ main.js
@@ -2,51 +2,51 @@
 let luckyNumber = Math.trunc(Math.random()*20+1);
 console.log(luckyNumber);
 
+const displayMessenger = function(messenger){
+    document.querySelector(".messenger").textContent = messenger;
+}
+
 // When inputNumber and Click in Check button
 document.querySelector('.check').addEventListener('click', function () {
 
     // create a variable to storage number input
     var inputNumber = Number(document.querySelector('.number').value);
-
     // Create a variable storage score
     const yourScore = document.querySelector('.score').textContent;
 
     if(inputNumber === luckyNumber){
 
         //Change messenger to ... and display lucky number 
-        document.querySelector(".messenger").textContent = '🥳 Correct number!'
+        displayMessenger('🥳 Correct number!');
         document.querySelector('.luckyNumber').textContent = luckyNumber;
 
         // compare High-score and score
         if(Number(document.querySelector('.highscore').textContent) < yourScore){
             document.querySelector('.highscore').textContent = document.querySelector('.score').textContent;
         }
-
         //Change background-color
         document.querySelector('.header').style.backgroundColor = '#60b347';
         document.querySelector('.main').style.backgroundColor = '#60b347';
         document.querySelector('.number').style.backgroundColor = '#60b347';
-    } else if(inputNumber >20){
+    } else if(inputNumber > 20){
 
         //Change messenger to...
-        document.querySelector(".messenger").textContent = '⚠️ Your number illegal'
+        displayMessenger('⚠️ Your number illegal');
     }else{
 
         // inputNumber > luckyNumber 
         if(inputNumber > luckyNumber){
-            document.querySelector(".messenger").textContent = '📉 Too Long !'
+            displayMessenger('📉 Too Long !');
         }
-
         // inputNumber < luckyNumber
         if(inputNumber < luckyNumber){
-            document.querySelector(".messenger").textContent = '📈 Too Low !'
+            displayMessenger('📉 Too Low !');
         }
-
         // Minus 5 point 
         if(yourScore > 0){
             document.querySelector('.score').textContent = Number(yourScore)-5;
         }else{
-            document.querySelector(".messenger").textContent = '🤡 You very stupid'
+            displayMessenger('🤡 You very stupid');
         }
     }        
 });
@@ -56,7 +56,6 @@ document.querySelector('.again').addEventListener('click', function(){
     luckyNumber = Math.trunc(Math.random()*20+1);
     document.querySelector('.luckyNumber').textContent = '?';
     document.querySelector('.score').textContent = 100;
-
     // 
     document.querySelector('.header').style.backgroundColor = 'black';
     document.querySelector('.main').style.backgroundColor = 'black';
